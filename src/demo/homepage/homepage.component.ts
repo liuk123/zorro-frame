4034f0bba6004f93259e0370df303ae81c2f4ad3
liuk123 <40844000+liuk123@users.noreply.github.com>
2020-05-17 21:40:11 +0800

    添加prime-zorro

--- src/demo/homepage/homepage.component.ts
+++ src/demo/homepage/homepage.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { NzMessageService } from 'ng-zorro-antd/message';
+import { MessageUtilService } from 'prime-zorro'
 
 @Component({
   selector: 'app-homepage',
@@ -11,16 +11,15 @@ export class HomepageComponent implements OnInit {
   title="";
   constructor(
     @Inject('CONFIG') private config,
-    private message: NzMessageService,
+    private messageUtil: MessageUtilService,
   ) { }
 
   ngOnInit() {
     // console.log(this.config.url)
-    // console.log(objectUtil.delNull(['', [], ' 123 ']))
-    // this.title=objectUtil.delNull(['', [], ' 123 ']);
   }
   createBasicMessage(): void {
-    this.message.info('This is a normal message');
+    this.messageUtil.success();
   }
 
 }
+
