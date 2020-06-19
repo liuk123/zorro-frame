6934bcc3d75f7b1af2eff9ba5c863e293cf6f1c5
liuk123 <liu___k@outlook.com>
2020-06-19 15:12:56 +0800

    简单修改

--- src/app/routes/routes-routing.module.ts
+++ src/app/routes/routes-routing.module.ts
@@ -22,10 +22,24 @@ const routes: Routes = [
   imports: [
     RouterModule.forRoot(
       routes, {
-        useHash: environment.useHash,
+        // useHash: environment.useHash,
         scrollPositionRestoration: 'top',
       }
     )],
   exports: [RouterModule],
 })
 export class RouteRoutingModule { }
+// router.events.pipe(
+//   *       filter((e: Event): e is Scroll => e instanceof Scroll)
+//   *     ).subscribe(e => {
+//   *       if (e.position) {
+//   *         // backward navigation
+//   *         viewportScroller.scrollToPosition(e.position);
+//   *       } else if (e.anchor) {
+//   *         // anchor navigation
+//   *         viewportScroller.scrollToAnchor(e.anchor);
+//   *       } else {
+//   *         // forward navigation
+//   *         viewportScroller.scrollToPosition([0, 0]);
+//   *       }
+//   *     });
